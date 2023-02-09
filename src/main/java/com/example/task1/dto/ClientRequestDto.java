0e006503ba30ca010b04fd55408227ea006ba631
kispetra <123375732+kispetra@users.noreply.github.com>
2023-02-09 13:31:41 +0100

    JIRA: us14 implement error handling

--- src/main/java/com/example/task1/dto/ClientRequestDto.java
+++ src/main/java/com/example/task1/dto/ClientRequestDto.java
@@ -1,15 +1,10 @@
 package com.example.task1.dto;
 
-import com.example.task1.model.Car;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter @Setter
 public class ClientRequestDto {
-    // TODO ne treba id u requestu
-    public String id;
     public String firstName;
     public String lastName;
     public String oib;
@@ -17,8 +12,7 @@ public class ClientRequestDto {
     public String street;
     public int zipCode;
     public String country;
+
     public String number;
 
-    // TODO cars ne smije biti ovdje, nigdje ne piše da trebaš slati listu auta u requestu
-    public List<Car> cars;
 }
